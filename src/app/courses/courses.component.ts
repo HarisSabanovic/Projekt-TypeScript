2a48e9f67228e367da692dd9dc00db1550a68a5a
Haris Sabanovic <hasa2303@student.miun.se>
2024-06-27 12:40:23 +0200

    skapade sökfiltrering, en dropdown som filtrerar tabellen beroende på vald ämne

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,25 +2,34 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
   courselist: Course[] = [];
+  filteredCourses: Course[] = [];
+  filterValue: string = "" ;
+  uniqueSubjects: string [] = [];
+  selectedSubject: string = "";
+  frameworkList: Course[] = [];
   
   constructor(private coursesservice : CoursesService) {}
 
   ngOnInit() {
     this.coursesservice.getCourses().subscribe(data => {
       this.courselist = data;
+      this.filteredCourses = data;
+      this.removeDups();
     })
-  };
+  }
 
   sortCourseCode() {
     this.courselist.sort((a,b) => a.courseCode.localeCompare(b.courseCode))
@@ -29,4 +38,29 @@ export class CoursesComponent {
   sortCourseName() {
     this.courselist.sort((a,b) => a.courseName.localeCompare(b.courseName))
   };
+
+  applyFilter(): void {
+    this.filteredCourses = this.courselist.filter((course) => 
+      course.courseName.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      course.courseCode.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+
+  removeDups(): void {
+    const subjectsSet = new Set<string>();
+    this.filteredCourses.forEach(course => subjectsSet.add(course.subject));
+    this.uniqueSubjects = Array.from(subjectsSet);
+  }
+
+  filterSubjects(): void {
+    this.filteredCourses = this.courselist.filter((course) => 
+      course.subject.toLowerCase().includes(this.selectedSubject.toLowerCase())
+    );
+  }
+  addcourse(course: Course): void {
+    if(!this.frameworkList.includes(course)) {
+      this.frameworkList.push(course);
+    }
+  }
+    
 }
