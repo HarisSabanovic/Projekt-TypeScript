d64dd56e3e361aea34024f480da9213292ffa626
Haris Sabanovic <hasa2303@student.miun.se>
2024-08-07 09:54:03 +0200

    lade till route till startsidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { FrameworkComponent } from './framework/framework.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { path: "", component: CoursesComponent },
+    { path: "", component: HomeComponent },
+    { path: "courses", component: CoursesComponent},
     { path: "framework", component: FrameworkComponent}
 ];
